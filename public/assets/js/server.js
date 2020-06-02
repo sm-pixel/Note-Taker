2edec2d8d907471781b9151216028851ded3428d
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-02 18:42:16 -0400

    added html routes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,17 +1,30 @@
 //dependencies
-const PORT = 4040;
-const express = require('express');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+const PORT = 4040;
 
-//root director that serve static assests
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //data
 
 //html routes
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'index.html'))
+});
+
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'notes.html'))
+});
 
 //api routes
 
+//use fs.writeFile to write notes to json
+
 //listener
 app.listen(4040, function() {
-    console.log("Listening on PORT " + PORT);
+    console.log("Listening on PORT " + PORT + " ...");
 });
