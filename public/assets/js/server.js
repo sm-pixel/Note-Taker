a4b7133d1d11048feccf02fcdf24d2010cfd5a71
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-02 18:46:58 -0400

    updated port path

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
-const PORT = 4040;
+const PORT = process.env.PORT || 4040;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
